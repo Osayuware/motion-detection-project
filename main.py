1d56579e06e533ea3d9e75567fdc9b3186a4322c
Osayuware <72750417+Osayuware@users.noreply.github.com>
2021-01-27 10:48:40 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,7 @@
+while True:
+    if input.rotation(Rotation.PITCH) > 150:
+        light.set_all(color.rgb(255,0,0))
+    else:
+        "Sound Level" + input.sound_level()
+        input.sound_level() > 150
+        music.siren.play_until_done()
---
+++ main.ts
@@ -0,0 +1,10 @@
+while (true) {
+    if (input.rotation(Rotation.Pitch) > 150) {
+        light.setAll(color.rgb(255, 0, 0))
+    } else {
+        "Sound Level" + input.soundLevel()
+        input.soundLevel() > 150
+        music.siren.playUntilDone()
+    }
+    
+}
